0c79bfe44f42b2eafef800423ba888eb67f88f63
TUD-RSmith <71321440+TUD-RSmith@users.noreply.github.com>
2023-10-16 09:38:35 +0100

    Initial commit

---
+++ Assets/MenuButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public string URL = "https://github.com/TUD-RSmith/Lecture_Demo/tree/main/Button_Demo";
+    public void GoToScene1()
+    {
+        SceneManager.LoadScene("scene1");
+    }
+
+    public void GoToScene2()
+    {
+        SceneManager.LoadScene("scene2");
+    }
+
+    public void loadUrl()
+    {
+        Application.OpenURL(URL);
+    }
+}
